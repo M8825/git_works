53e0f6014de8b2622d70043262968eb0be2a6871
MkMula <mk@MkMulas-MacBook-Pro.local>
2022-08-29 19:51:50 -0400

    indexing

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-console.log("Hello World")
+// console.log(10 > 10);                        // false
+// console.log(10 >= 10);                       // true
+// console.log(10 < 11);                        // true
+// console.log(10 <= 10);                       // true
+// console.log(10 === 10);                      // true
+// console.log(10 !== 11);                      // true
+// console.log(0 === -0);                       // true
+// console.log(false !== true);                 // true
 
-console.log("FooBar")
+
+// 1. Indexing - Returns the character at certain position
+// Way to access our data
+
+console.log("hello"[0]);                     // "h"
+console.log("hello"[1]);                     // "e"
+console.log("hello"[2]);                     // "l"
+console.log("hello"[6]);                     // undefined; default value when no value present
+console.log("hi bye"[2]);                    // " "
